16415b97f5e507b52e29521673aac167cb991b47
Arikaim <info@arikaim.com>
2022-10-24 20:33:15 +0300

    =

--- Preloader.php
+++ Preloader.php
@@ -21,12 +21,22 @@ use FilesystemIterator;
 class Preloader
 {
     /**
+     * Show output
+     *
+     * @var bool
+     */
+    private $verbose;
+
+    /**
      * Constructor
      *
      * @param array $files
+     * @param bool $verbose
      */
-    public function __construct(array $files = [])
+    public function __construct(array $files = [], bool $verbose = false)
     {
+        $this->verbose = $verbose;
+
         foreach ($files as $file) {
             $this->load($file);
         }
@@ -42,10 +52,17 @@ class Preloader
     {
         if (\is_dir($file)) {
             $this->loadPath($file);
-        } else {
-            require_once($path);
+            return $this;
+        } 
+        
+        if (\file_exists($file) == false) {
+            $this->message('File not exist: ' . $file);
+            return $this;
         }
 
+        require_once($file);
+        $this->message('File loaded: ' . $file);           
+    
         return $this;
     }
 
@@ -61,8 +78,9 @@ class Preloader
         $iterator = new RecursiveIteratorIterator($dir);
         $phpFiles = new RegexIterator($iterator,'/^.+\.php$/i',RecursiveRegexIterator::GET_MATCH);
 
-        foreach ($phpFiles as $file) {              
-            require_once($file[0]);
+        foreach ($phpFiles as $file) { 
+            require_once($file[0]);    
+            $this->message('File loaded: ' . $file[0]);
         }
     }
 
@@ -74,5 +92,18 @@ class Preloader
     public static function getCachedFiles(): array
     {
         return \opcache_get_status()['scripts'] ?? [];
+    }
+
+    /**
+     * Show message
+     *
+     * @param string $message
+     * @return void
+     */
+    protected function message(string $message): void
+    {
+        if ($this->verbose == true) {
+            echo $message . "\n";
+        }
     }
 }
